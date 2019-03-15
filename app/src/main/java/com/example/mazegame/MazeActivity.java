caf62ef3f2690bbe28b468a776a045a4633a4239
David Pojunas <davidapojunas@gmail.com>
2019-03-14 20:42:22 -0500

    ftcghj

--- app/src/main/java/com/example/mazegame/MazeActivity.java
+++ app/src/main/java/com/example/mazegame/MazeActivity.java
@@ -1,7 +1,13 @@
 package com.example.mazegame;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MazeActivity extends AppCompatActivity {
 
@@ -9,5 +15,8 @@ public class MazeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
+
     }
+
+
 }
