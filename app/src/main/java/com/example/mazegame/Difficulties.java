5dd016ade472fe8f5b8b9ff09a17d225c79578c4
RaderF <36457068+RaderF@users.noreply.github.com>
2019-04-02 17:04:26 -0500

    Updated buttons and improved other screens
    Difficulties, ScorePage, activity_difficulties all updated with (hopefully) working buttons.

--- app/src/main/java/com/example/mazegame/Difficulties.java
+++ app/src/main/java/com/example/mazegame/Difficulties.java
@@ -1,22 +1,75 @@
 package com.example.mazegame;
-
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-enum Difficulty {Easy, Medium, Hard};
-
 public class Difficulties extends Activity {
-
     private Button Easy;
     private Button Medium;
     private Button Hard;
 
 
-    public void findIDs(){
-        //setContentView(R.layout.activity_difficulties);
-        //toEasy = findViewById(R.id.toEasy);
-        //toDifficult = findViewById(R.id.toMedium);
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulties);
+        findIDs();
+        toEasyButton();
+        toMediumButton();
+        toHardButton();
     }
+
+    public void findIDs(){
+        setContentView(R.layout.activity_difficulties);
+        Easy = findViewById(R.id.toEasy);
+        Medium = findViewById(R.id.toMedium);
+        Hard = findViewById(R.id.toHard);
+    }
+
+    private void toEasyButton() {
+        Easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toEasyActivity();
+            }
+        });
+    }
+
+    private void toEasyActivity() {
+        Intent forwardIntent = new Intent(Difficulties.this, MazeActivity.class);
+        startActivity(forwardIntent);
+    }
+
+    private void toMediumButton() {
+        Medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toMediumActivity();
+            }
+        });
+    }
+
+    private void toMediumActivity() {
+        Intent forwardIntent = new Intent(Difficulties.this, MazeActivity.class);
+        startActivity(forwardIntent);
+    }
+
+    private void toHardButton() {
+        Hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toHardActivity();
+            }
+        });
+    }
+
+    private void toHardActivity() {
+        Intent forwardIntent = new Intent(Difficulties.this, MazeActivity.class);
+        startActivity(forwardIntent);
+    }
+
+
 
 }
--- app/src/main/java/com/example/mazegame/ScorePage.java
+++ app/src/main/java/com/example/mazegame/ScorePage.java
@@ -6,12 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import java.util.Dictionary;
 
 public class ScorePage extends Activity {
     private Dictionary high_scores;
+    private Spinner score_display;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+        findIDs();
 
+    }
+
+    public void findIDs(){
+        setContentView(R.layout.activity_scores);
+    }
 
 }
