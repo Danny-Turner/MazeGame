fcc06213bdd1d264d1d03e99c925a32c4142ab61
RaderF <36457068+RaderF@users.noreply.github.com>
2019-04-03 11:14:41 -0500

    Updated ScorePage
    renovated design philosophy, added a test/button

--- app/src/main/java/com/example/mazegame/ScorePage.java
+++ app/src/main/java/com/example/mazegame/ScorePage.java
@@ -1,29 +1,47 @@
 package com.example.mazegame;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 
-import java.util.Dictionary;
+import java.util.ArrayList;
 
 public class ScorePage extends Activity {
-    private Dictionary high_scores;
-    private Spinner score_display;
+    private ArrayList<Pair<String, String>> HighScores;
+    private Button addScoreTestButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
         findIDs();
-
+        addScoreTestButton();
     }
 
-    public void findIDs(){
+    private void findIDs(){
         setContentView(R.layout.activity_scores);
+        addScoreTestButton = findViewById(R.id.addScoreTestButton);
     }
+
+
+    private void addScoreTestButton(){
+        addScoreTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addScoreTest();
+            }
+        });
+    }
+
+    private void addScoreTest(){
+        String playername = "Rader";
+        String playerscore = "5:32";
+        HighScores.add(Pair.<String, String>create(playername, playerscore));
+    }
+
+
 
 }
