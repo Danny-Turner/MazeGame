340a26e85e52a8fb168da5da0e6b66a2b9b204e5
RaderF <36457068+RaderF@users.noreply.github.com>
2019-04-08 17:26:53 -0500

    More score stuff
    Added ScoreCreator and Activity_recorder for it, will finish implementation soon

---
+++ app/src/main/java/com/example/mazegame/ScoreCreator.java
@@ -0,0 +1,20 @@
+package com.example.mazegame;
+
+import android.app.Activity;
+
+public class ScoreCreator extends Activity {
+
+    String playerName;
+    int playerScore;
+    int playerDifficulty;
+
+
+    public ScoreCreator() {
+    setPlayerName();
+    }
+
+    private void setPlayerName() {
+       // this.playerName =
+    }
+
+}
