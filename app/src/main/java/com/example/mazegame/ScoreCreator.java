1d83664737972c35e43b5727ac760216d4db0cd7
RaderF <36457068+RaderF@users.noreply.github.com>
2019-04-10 08:55:19 -0500

    Working update to high score creation
    more tuning to classes relevant to high scores, including a refactor to make the two different score related xml files be more distinct

--- app/src/main/java/com/example/mazegame/ScoreCreator.java
+++ app/src/main/java/com/example/mazegame/ScoreCreator.java
@@ -1,20 +1,51 @@
 package com.example.mazegame;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ScoreCreator extends Activity {
-
-    String playerName;
-    int playerScore;
+    long playerScore;
     int playerDifficulty;
+    private Button submit_button;
+    private EditText name_input_box;
 
-
-    public ScoreCreator() {
-    setPlayerName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        findIDs();
+        submit_score_button();
     }
 
-    private void setPlayerName() {
-       // this.playerName =
+
+    public void findIDs(){
+        setContentView(R.layout.new_score_activity);
+        submit_button = findViewById(R.id.submit_button);
+        name_input_box = findViewById(R.id.name_input_box);
     }
+
+
+    private void submit_score_button() {
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // creates and adds a new high score with the value from the editText box and from the given score.
+                // sends user to mainActivity
+                ScorePage.addNewScore(new HighScore(name_input_box.getText().toString()+playerDifficulty,playerScore));
+                toMainActivity();
+            }
+        });
+    }
+
+
+    private void toMainActivity() {
+        Intent forwardIntent = new Intent(ScoreCreator.this, MainActivity.class);
+        startActivity(forwardIntent);
+    }
+
 
 }
--- app/src/main/java/com/example/mazegame/ScorePage.java
+++ app/src/main/java/com/example/mazegame/ScorePage.java
@@ -2,7 +2,6 @@ package com.example.mazegame;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,13 +15,13 @@ public class ScorePage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scores);
+        setContentView(R.layout.high_score_page_activity);
         findIDs();
         addScoreTestButton();
     }
 
     private void findIDs(){
-        setContentView(R.layout.activity_scores);
+        setContentView(R.layout.high_score_page_activity);
         addScoreTestButton = findViewById(R.id.addScoreTestButton);
     }
 
